d6fd9703f2a74494913cfa3aebd0e943c9693bb5
xtalo <117736097+santiagoieshna@users.noreply.github.com>
2023-10-06 17:48:36 +0200

    ASP ProyectoUno

---
+++ Dev_int_Proyects/ASP.NET/ProyectoUno_Santi/ProyectoUno_Santi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoUno_Santi.MvcApplication" Language="C#" %>
---
+++ Dev_int_Proyects/ASP.NET/ProyectoUno_Santi/ProyectoUno_Santi/Models/Libro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoUno_Santi.Models
+{
+    public class Libro
+    {
+        private String ISBN;
+        private String titulo;
+        private String autor;
+        private String editorial;
+        private int stock;
+        private float precio;
+
+        public Libro()
+        {
+
+        }
+
+
+    }
+}
